b90dddcb7ce8b97980dda8db111efcbff4e040e3
sowmik-sec <sowmik.sec@gmail.com>
2023-03-16 12:45:42 +0600

    favicon, title changed and home component created

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home";
 import Main from "../layout/Main/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    children: [{}],
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 export default router;
