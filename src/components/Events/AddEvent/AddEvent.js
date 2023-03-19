d4eefdcacbba46a256eff288c1d5ad4949c6d5ad
sowmik-sec <sowmik.sec@gmail.com>
2023-03-19 21:59:47 +0600

    taking input data from user and sending it to server side

--- src/components/Events/AddEvent/AddEvent.js
+++ src/components/Events/AddEvent/AddEvent.js
@@ -1,19 +1,46 @@
+import axios from "axios";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const AddEvent = () => {
-  const [title, setTitle] = useState("");
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [description, setDescription] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    date: "",
+    description: "",
+    file: null,
+  });
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const handleChange = (event) => {
+    const { name, value, files } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: files ? files[0] : value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const url = "http://localhost:5000/events";
+    const formDataToSend = new FormData();
+    formDataToSend.append("title", formData.title);
+    formDataToSend.append("date", formData.date);
+    formDataToSend.append("description", formData.description);
+    formDataToSend.append("file", formData.file);
+    fetch(url, {
+      method: "POST",
+      body: formDataToSend,
+    })
+      .then((response) => {
+        // Handle response from server-side
+      })
+      .catch((error) => {
+        // Handle error
+      });
   };
   return (
     <div className="bg-slate-100 min-h-screen">
-      <form className="">
+      <form className="" onSubmit={handleSubmit}>
         <div className="md:flex md:justify-evenly">
           <div className="form-control w-[45%]">
             <label className="label">
@@ -21,8 +48,10 @@ const AddEvent = () => {
             </label>
             <input
               type="text"
+              name="title"
               placeholder="Event Title"
-              onChange={(event) => setTitle(event.target.value)}
+              value={formData.title}
+              onChange={handleChange}
               className="input input-bordered "
             />
           </div>
@@ -30,11 +59,13 @@ const AddEvent = () => {
             <label className="label">
               <span className="label-text">Event Date</span>
             </label>
-            <DatePicker
-              selected={selectedDate}
-              onChange={(date) => setSelectedDate(date)}
-              dateFormat="dd/MM/yyyy"
-              minDate={new Date()}
+            <input
+              type="date"
+              name="date"
+              id="date"
+              value={formData.date}
+              onChange={handleChange}
+              min={`${new Date().toISOString().split("T")[0]}`}
               className="w-full border border-gray-200 rounded-lg p-2"
             />
           </div>
@@ -47,7 +78,9 @@ const AddEvent = () => {
             <textarea
               className="textarea textarea-bordered"
               placeholder="Write Description"
-              onChange={(event) => setDescription(event.target.value)}
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
             ></textarea>
           </div>
           <div className="form-control file-input-container w-[45%]">
@@ -56,14 +89,17 @@ const AddEvent = () => {
             </label>
             <input
               type="file"
-              onChange={handleFileChange}
+              onChange={handleChange}
+              name="file"
               id="file-input"
               className="file-input"
             />
           </div>
         </div>
         <div className="text-right">
-          <button className="btn btn-primary mr-8 my-5">Add Event</button>
+          <button className="btn btn-primary mr-8 my-5" type="submit">
+            Add Event
+          </button>
         </div>
       </form>
     </div>
@@ -71,3 +107,14 @@ const AddEvent = () => {
 };
 
 export default AddEvent;
+
+/*
+
+<DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="dd/MM/yyyy"
+              minDate={new Date()}
+              className="w-full border border-gray-200 rounded-lg p-2"
+            />
+*/
